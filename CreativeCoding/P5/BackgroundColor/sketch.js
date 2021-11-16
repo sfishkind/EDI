72881cf8fcc8738577f558a06065eeb7d4bf5563
scarlet <sfishkind22@gmail.com>
2021-11-16 14:06:59 -0500

    color change

---
+++ CreativeCoding/P5/BackgroundColor/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+createCanvas (windowWidth, windowHeight); // this creates the window W/H to be the size of my browser window
+background (255,0,0); // red background
+}
+
+function draw() {
+
+}
