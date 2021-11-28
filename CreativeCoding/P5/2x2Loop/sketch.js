def99372ce82cddec974e4af03940344c7db302c
scarlet <sfishkind22@gmail.com>
2021-11-28 14:16:50 -0500

    2 by 2 loop

---
+++ CreativeCoding/P5/2x2Loop/sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(720, 720);
+  fill ('purple');
+  strokeWeight(8);
+}
+
+function draw() {
+  background(220);
+  
+  translate (60,60);
+  
+  
+  for (var y = 0; y < 600; y = y + 300) {  // loop to create rows in the y direction
+    
+  
+  for (var x = 0; x < 600; x = x + 300) { // loop to create a row of squares in the x direction
+    
+      quad (x, y,
+        x + 300, y, 
+        x + 300, y + 300,
+        x, y + 300); 
+  }
+    
+  }
+  
+
+  
+
+  
+  noLoop();
+}
