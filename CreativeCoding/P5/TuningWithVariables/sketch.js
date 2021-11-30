4a96c54f2f53add4eb0e8412d6a2c43071231014
scarlet <sfishkind22@gmail.com>
2021-11-30 16:14:37 -0500

    tuning with variables

---
+++ CreativeCoding/P5/TuningWithVariables/sketch.js
@@ -0,0 +1,32 @@
+function setup() {
+  createCanvas(windowWidth, windowWidth);
+  fill ('green');
+  strokeWeight(1);
+}
+
+function draw() {
+
+var num = 10; // variable for the number of squares in the array
+  
+  var sideLen = windowWidth / num; // variable for the side length of each square
+
+
+  for (var y = 0; y < windowWidth; y = y + sideLen) {  // loop to create rows in the y direction
+
+
+  for (var x = 0; x < windowWidth; x = x + sideLen) { // loop to create a row of squares in the x direction
+
+      quad (x, y,
+        x + sideLen, y,
+        x + sideLen, y + sideLen,
+        x, y + sideLen);
+  }
+
+  }
+
+
+
+
+
+  noLoop();
+}
