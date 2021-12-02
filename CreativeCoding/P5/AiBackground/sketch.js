874f1327ad735803d2a3564ca1971b946c56ad96
scarlet <sfishkind22@gmail.com>
2021-12-02 10:14:25 -0500

    push

--- CreativeCoding/P5/AiBackground/sketch.js
+++ CreativeCoding/P5/AiBackground/sketch.js
@@ -1,7 +1,34 @@
-function setup() {
 
+function setup() {
+  createCanvas(720, 720);
+  fill ('yellow');
+  strokeWeight(8);
+}
+
+let img;// declare image variable
+
+function preload () {
+img = loadImage ('assets/AiBGsingle.png');
 }
 
 function draw() {
 
+
+  background(220);
+
+  translate (60,60);
+
+
+  for (var y = 0; y < 600; y = y + 300) {  // loop to create rows in the y direction
+
+
+  for (var x = 0; x < 600; x = x + 300) { // loop to create a row of squares in the x direction
+  image (img, x, y);
+
+  }
+
+  }
+
+
+  noLoop();
 }
