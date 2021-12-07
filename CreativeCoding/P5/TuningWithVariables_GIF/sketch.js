4fe4bd896de32776afa63b93956e543e3518a8c4
scarlet <sfishkind22@gmail.com>
2021-12-07 08:04:48 -0500

    ok

--- CreativeCoding/P5/TuningWithVariables_GIF/sketch.js
+++ CreativeCoding/P5/TuningWithVariables_GIF/sketch.js
@@ -1,32 +1,27 @@
+let gif;
+
+function preload() {
+  gif = loadImage ('untitled folder/AiBG.gif');
+}
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  fill ('green');
-  strokeWeight(1);
 }
 
 function draw() {
+background(0,0,0);
 
-var num = 10; // variable for the number of squares in the array
 
-  var sideLen = windowWidth / num; // variable for the side length of each square
+var num = 5; // variable for the number of squares in the array
+var sideLen = windowWidth / num; // variable for the side length of each square
 
 
   for (var y = 0; y < windowHeight; y = y + sideLen) {  // loop to create rows in the y direction
 
 
   for (var x = 0; x < windowWidth; x = x + sideLen) { // loop to create a row of squares in the x direction
-
-      quad (x, y,
-        x + sideLen, y,
-        x + sideLen, y + sideLen,
-        x, y + sideLen);
+  image(gif,x,y);
   }
 
   }
 
-
-
-
-
-  noLoop();
 }
