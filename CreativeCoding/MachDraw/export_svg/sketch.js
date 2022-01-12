562ff6f139d4f784bf28978e8d8b40bb77b67fbf
scarlet <sfishkind22@gmail.com>
2022-01-12 13:51:40 -0500

    uigiu

--- CreativeCoding/MachDraw/export_svg/sketch.js
+++ CreativeCoding/MachDraw/export_svg/sketch.js
@@ -4,21 +4,28 @@ need to have p5.svg.js in project and in index.html
 see -https://github.com/zenozeng/p5.js-svg
 this will save an SVG file in your download folder
 */
+var pW = 11; // width of paper in inches
+var pH = 8.5; // height of the paper in inches
+
 
 function setup() {
-  createCanvas(200, 200, SVG); // Create SVG Canvas
+  createCanvas(pW * 72, pH * 72, SVG); // Create SVG Canvas
   strokeWeight(1); // do 0.1 for laser
   stroke(255, 0, 0); // red is good for laser
   noFill(); // better not to have a fill for laser
+
 }
 
 function draw() {
-  for (x = 0; x < 10; x++) {
-    for (y = 0; y < 10; y++) {
-      rect(x * 10, y * 10, x, y);
-    }
+  translate (((pW * 72) - 600) / 2, ((pH * 72) - 600) / 2);
+  for (var y = 0; y < 600; y = y + random (100, 50)) {
+  for (var x = 0; x < 600; x = x + random (100, 50)) {
+
+    triangle (x,y,random (100, 50),random (100, 50));
   }
-  save("mySVG.svg"); // give file name
-  print("saved svg");
+  }
+
+save("mySVG.svg"); // give file name
+print("saved svg");
   noLoop(); // we just want to export once
 }
